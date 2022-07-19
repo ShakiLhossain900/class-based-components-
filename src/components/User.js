1529df4246447ecb268a833c660aff2d8d874f34
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-20 00:16:27 +0600

    adding

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,18 @@
 import classes from './User.module.css';
+import {Component} from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
+// this is not a javascript concept this is mordern javascript concept
+// this render method to equvalent to return stement in a functional components
+//props proparty can access in class based componts that time need to use this
+class User extends Component {
+    render() {
+        return <li className={classes.user}>{this.props.name}</li>; 
+    }
 };
 
+// const User = (props) => {
+//     return <li className={classes.user}>{props.name}</li>;
+// };
+
 export default User;
+
