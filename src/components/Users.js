02c7bec2a25a99359b0000802b046d9ea1560cb1
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-19 09:08:45 +0600

    adeding

---
+++ src/components/Users.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Users = () => {
+    const DUMMY_USERS = [
+        { id: 'u1', name: 'Max' },
+        { id: 'u2', name: 'Manuel' },
+        { id: 'u3', name: 'Julie' },
+      ];      
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Users;
